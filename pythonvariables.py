9319660747ce4c1a8cac22c13fcea836bb0a54b7
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2024-02-16 05:31:28 -0500

    exercise 3 solution for python variables.

--- pythonvariables.py
+++ pythonvariables.py
@@ -9,3 +9,8 @@ print(carname)
 #Create a variable named x and assign the value 50 to it.
 x = 50
 print(x)
+#exercise 3
+#Display the sum of 5 + 10, using two variables: x and y.
+x = 5
+y=10
+print(x+y)
