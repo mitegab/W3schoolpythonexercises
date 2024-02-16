8efc5d19dca8ac71e47f152b830d3effe79f4f7b
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2024-02-16 05:13:24 -0500

    python syntax exercise solutions

--- ex1.py
+++ ex1.py
@@ -1,4 +1,17 @@
+#python syntax exercises
+
+#exercise 1.
 #Insert the missing part of the code below to output "Hello World".
+
+
 
 print("Hello World")
 # Output: Hello World
+
+#exercise 2.
+
+""" Complete the code block, print "YES" if 5 is larger than 2.
+
+Hint: remember the indentation. """
+if 5 > 2:
+    print("YES")
