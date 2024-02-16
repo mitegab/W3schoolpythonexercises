8b06f2c1adda2b4085c42cc9bc2319b163b7483b
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2024-02-16 05:29:19 -0500

    exercise two solution for python variables.

--- pythonvariables.py
+++ pythonvariables.py
@@ -5,3 +5,7 @@ this one is all about phyton variables how to use them and how to declare them
 #Create a variable named carname and assign the value Volvo to it.
 carname = "Volvo"
 print(carname)
+#exercise 2
+#Create a variable named x and assign the value 50 to it.
+x = 50
+print(x)
